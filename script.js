1fe0abe978ec8dae7cea2aaa95624f9402936315
joao.silva <joao.silva@arapiraca.al.gov.br>
2019-09-28 14:02:05 -0300

    Pegando root element com

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ Vue.component('modal',{
 
     props: ['timeCasa', 'timeFora'],
     template: `
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" tabindex="-1" role="dialog">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -153,11 +153,11 @@ Vue.component('modal',{
     methods: {
 
         show(){
-            $('#exampleModal').modal('show');
+            $(this.$el).modal('show');
         },
 
         close(){
-            $('#exampleModal').modal('hide');
+            $(this.$el).modal('hide');
         },
 
         fimJogo(){
